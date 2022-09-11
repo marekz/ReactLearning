9a59bd1cb416580d64a11bf19e094af36e041076
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-09-11 18:49:34 +0200

    access to component properties and methods

--- podstawy/src/App.js
+++ podstawy/src/App.js
@@ -1,10 +1,22 @@
 import React, {Component} from "react";
 
-const message = "To jest treść";
-
 export default class App extends Component {
-  render = () =>
-      <h4 className="bg-primary text-white text-center p-2 m-1">
-          { message }
-      </h4>
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 4
+        }
+    }
+
+    isEven(val) {
+        return val % 2 === 0 ? "parzysta" : "nieparzysta";
+    }
+
+    render = () =>
+        <p className="bg-primary text-white text-center p-2 m-1">
+            Liczba przedniotów jest { this.isEven(this.state.count) }
+        </p>
+
 }
