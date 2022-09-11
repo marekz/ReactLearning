624a693725d67f3cc86bd202a9d4896868d3d7aa
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-09-11 18:35:58 +0200

    Some cleaning

--- src/TodoBanner.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export class TodoBanner extends Component {
-    render = () =>
-        <h4 className="bg-primary text-white text-center p-2">
-            Lista zadań użytjownika { this.props.name }
-            (Liczba zadań: { this.props.tasks.filter(t => !t.done).length })
-        </h4>
-}
