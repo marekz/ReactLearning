0635cff71401547da51355686a0f05f5423a5796
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-09-11 19:23:30 +0200

    Use bootstrap with tables

--- podstawy/src/App.js
+++ podstawy/src/App.js
@@ -5,12 +5,12 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 5
+            count: 1
         }
     }
 
     isEven(val) {
-        return val % 2 === 0 ? "parzysta" : "nieparzysta";
+        return val % 2 === 0 ? "Tak" : "Nie";
     }
 
     getClassName(val) {
@@ -22,22 +22,28 @@ export default class App extends Component {
     handleClick = () => this.setState({ count: this.state.count + 1 });
 
     render = () =>
-        <div className="container-fluid p-4">
-            <div className="row bg-info text-white p-2">
-                <div className="col font-weight-bold">Wartość</div>
-                <div className="col-6 font-weight-bold">Parzysta</div>
-            </div>
-            <div className="row bg-light p-2 border">
-                <div className="col">{ this.state.count }</div>
-                <div className="col-6">{ this.isEven(this.state.count) }</div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <button className="btn btn-info m-2"
-                            onClick={this.handleClick}>
-                        Kliknij mnie
-                    </button>
-                </div>
-            </div>
-        </div>
+        <table className="table table-striped table-bordered table-sm">
+            <thead className="bg-info text-white">
+                <tr>
+                    <th>Wartość</th>
+                    <th>Parzysta</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{ this.state.count }</td>
+                    <td>{ this.isEven(this.state.count) }</td>
+                </tr>
+            </tbody>
+            <tfoot className="text-center">
+                <tr>
+                    <td colSpan="2">
+                        <button className="btn btn-info m-2"
+                                onClick={ this.handleClick }>
+                            Kliknij mnie...
+                        </button>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
 }
