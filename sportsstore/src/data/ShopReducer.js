dc256c50d64d4331f7107fa69f9af972e14eb766
Marek Zdybel <marek@motta.com.pl>
2022-09-24 19:55:27 +0200

    Type on ShopReducer

--- sportsstore/src/data/ShopReducer.js
+++ sportsstore/src/data/ShopReducer.js
@@ -1,8 +1,8 @@
-import { ActionType } from "./Types";
+import { ActionTypes } from "./Types";
 
 export const ShopReducer = (storeData, action) => {
     switch(action.type) {
-        case ActionType.DATA_LOAD:
+        case ActionTypes.DATA_LOAD:
             return {
                 ...storeData,
                 [action.payload.dataType]: action.payload.data
