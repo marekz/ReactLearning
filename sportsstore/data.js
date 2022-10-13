ee331efefebce1215138f383ea1dfbbc7805deae
Marek Zdybel <marek@motta.com.pl>
2022-10-13 20:20:57 +0200

    Using faker to generate more fake product records

--- sportsstore/data.js
+++ sportsstore/data.js
@@ -1,17 +1,22 @@
+var faker = require("faker");
+var data = [];
+var categories = ["Sporty wodne" ,"Piłka nożna", "Szachy", "Bieganie"];
+faker.seed(100);
+for (let i = 1; i <= 503; i++) {
+    var category = faker.helpers.randomize(categories);
+    data.push({
+        id: i,
+        name: faker.commerce.productName(),
+        category: category,
+        description: `${category}: ${faker.lorem.sentence(3)}`,
+        price: Number(faker.commerce.price())
+    })
+}
+
 module.exports = function () {
     return {
-        categories: ["Sporty wodne" ,"Piłka nożna", "Szachy"],
-        products: [
-            { id: 1, name: "Kajak", category: "Sporty wodne", description: "Jednoosobowa łódka", price: 990 },
-            { id: 2, name: "Kamizelka ratunkowa", category: "Sporty wodne", description: "Pewna i bezpieczna", price: 79.95 },
-            { id: 3, name: "Piłka nożna", category: "Piłka nożna", description: "Zatwierdzona przez FIFA", price: 220.50},
-            { id: 4, name: "Flagi do narożników boiska", category: "Piłka nożna", description: "Takie tam dekoracje", price: 450.99},
-            { id: 5, name: "Stadion", category: "Piłka nożna", description: "Stadion na 35000 miejsc", price: 1795000},
-            { id: 6, name: "Berecik ganiusza", category: "Szachy", description: "POprawia działanie szarych komórek", price: 129},
-            { id: 7, name: "Niesforne włosy", category: "Szachy", description: "Rozpraszacze przeciwnika", price: 89},
-            { id: 8, name: "Ludzka szachownica", category: "Szachy", description: "Śmieszna zabawa dla całej rodziny", price: 230},
-            { id: 9, name: "Błyszczący król", category: "Szachy", description: "Dekoracyjny, pozłacany i wysadzany diamentami KRÓL", price: 3999}
-        ],
+        categories: categories,
+        products: data,
         orders: []
     }
 }
