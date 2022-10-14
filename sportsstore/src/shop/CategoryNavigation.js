9670884d5d7adeae8c75a3b2da0137191a0ca34f
Marek Zdybel <marek.zdybel@gmail.com>
2022-10-14 20:33:39 +0200

    Update "all categories" button

--- sportsstore/src/shop/CategoryNavigation.js
+++ sportsstore/src/shop/CategoryNavigation.js
@@ -4,7 +4,9 @@ import { ToggleLink } from "../ToggleLink";
 export class CategoryNavigation extends Component {
     render() {
         return <React.Fragment>
-            <ToggleLink to={this.props.baseUrl} exact={true}>Wszystkie</ToggleLink>
+            <ToggleLink to={`${this.props.baseUrl}/wszystkie`} exact={false}>
+                Wszystkie
+            </ToggleLink>
             {this.props.categories && this.props.categories.map(cat =>
                 <ToggleLink key={cat}
                 to={`${this.props.baseUrl}/${cat.toLowerCase()}`}>
