4925cf862f3402f1334a2f87d5be6ec43899fda4
Marek Zdybel <marek@motta.com.pl>
2023-01-04 19:34:55 +0100

    define simple component with HTML Content 2

--- components/src/App.js
+++ components/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function App() {
-  return <h1 className="bg-primary text-white text-center p-2">Witaj Adminie!</h1>
+  return (
+      <h1 className="bg-primary text-white text-center p-2">
+        Witaj Adminie!
+      </h1>
+  )
+
 }
