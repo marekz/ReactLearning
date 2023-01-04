9430373e38f80c0819cdf6e639ad6d6418243992
Marek Zdybel <marek@motta.com.pl>
2023-01-04 20:33:56 +0100

    Selective content rendering

--- components/src/Message.js
+++ components/src/Message.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 export function Message(props) {
-    let classes = props.name === "Jacku" ? "bg-warning p-2"
-        : "bg-success text-white text-center p-2";
+    let classes;
+
+    switch (props.name) {
+        case "Jacku":
+            classes = "bg-warning p-2";
+            break;
+        case "Marku":
+            classes = "bg-secondary text-white text-center p-2";
+            break;
+        default:
+            classes = "bg-success text-white text-center p-2"
+    }
 
     return <h4 className={ classes }>
         {props.graeting}, {props.name} !
