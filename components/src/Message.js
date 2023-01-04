61f67322ce552cc9f228ec80cdaecfd152ccf900
Marek Zdybel <marek@motta.com.pl>
2023-01-04 20:26:34 +0100

    Selective content rendering

--- components/src/Message.js
+++ components/src/Message.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 export function Message(props) {
+    if (props.name === "Jacku") {
+        return <h4 className="bg-warning p-2">
+            {props.graeting}, {props.name} !
+        </h4>
+    }
     return <h4 className="bg-success text-white text-center p-2">
         {props.graeting}, {props.name} !
     </h4>
