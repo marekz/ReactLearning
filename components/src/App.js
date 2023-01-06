13285e24ab0d170a8dfe93e734f5aaed1719cd63
Marek Zdybel <marek@motta.com.pl>
2023-01-06 19:10:34 +0100

    Using array to render content in component [example]

--- components/src/App.js
+++ components/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Message } from "./Message";
+// import { Message } from "./Message";
 import { Summary } from "./Summary";
 
 export default function App() {
@@ -8,10 +8,7 @@ export default function App() {
             <h1 className="bg-primary text-white text-center p-2">
                 Witaj Adminie!
             </h1>
-            <Message graeting="Witaj" name="Jacku" />
-            <Message graeting="Siema" name={"Alu " + "Zuch"} />
-            <Message graeting="Cześć" name="Marku" />
-            <Summary />
+            <Summary names={["Jarek", "Ala", "Marek"]} />
         </div>
     )
 }
--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-export function Summary() {
+function createInnerElement(names) {
+    let arrayElement = [];
+    for (let i = 0; i < names.length; i++ ) {
+        arrayElement.push(
+            <div>
+                {`"${names[i]}" składs się z ${names[i].length} znaków`}
+            </div>
+        )
+    }
+
+    return arrayElement;
+}
+
+export function Summary(props) {
     return <h4 className="bg-info text-white text-center p-2">
-        TO jest podsumowanie!
+        { createInnerElement(props.names) }
     </h4>
 }
