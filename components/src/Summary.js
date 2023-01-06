0739edd2ef8dcc6438dee4f703ba1d95fe9056df
Marek Zdybel <marek@motta.com.pl>
2023-01-06 19:20:12 +0100

    example Simplifing

--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-function createInnerElement(names) {
-    return names.map(name =>
-        <div>
-            {`"${name}" składs się z ${name.length} znaków`}
-        </div>
-    );
-}
-
 export function Summary(props) {
     return <h4 className="bg-info text-white text-center p-2">
-        { createInnerElement(props.names) }
+        {
+            props.names.map(name =>
+                <div>
+                    {`"${name}" składs się z ${name.length} znaków`}
+                </div>
+            )
+        }
     </h4>
 }
