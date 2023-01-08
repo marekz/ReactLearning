657ddb6f41a1d09de71a862a82923cff2bc9cb26
Marek Zdybel <marek@motta.com.pl>
2023-01-08 13:45:13 +0100

    passing props to child components

--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {CallbackButton} from "./CallbackButton";
 
 export function Summary(props) {
     return <React.Fragment>
@@ -6,14 +7,12 @@ export function Summary(props) {
         <td>{props.name} </td>
         <td>{props.name.length} </td>
         <td>
-            <button className="btn btn-primary btn-sm"
-                onClick={props.reverseCallback}>
-                Zmień kolejność
-            </button>
-            <button className="btn btn-info btn-sm m-1"
-                    onClick={() => props.promoteCallback(props.name)}>
-                Na początek
-            </button>
+            <CallbackButton theme="primary"
+                text="Zmień kolejność"
+                callback={props.reverseCallback} />
+            <CallbackButton theme="info"
+                text="Na początek"
+                callback={() => props.promoteCallback(props.name)} />
         </td>
     </React.Fragment>
 }
