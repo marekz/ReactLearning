cedaea6469de4fba2104e2c82f677fb38bab3acb
Marek Zdybel <marek@motta.com.pl>
2023-01-08 14:23:28 +0100

    Selectively passing props to child components

--- components/src/CallbackButton.js
+++ components/src/CallbackButton.js
@@ -2,7 +2,9 @@ import React from "react";
 import {SimpleButton} from "./SimpleButton";
 
 export function CallbackButton(props) {
+    let { theme, ...childProps } = props;
+
     return (
-        <SimpleButton {...props} className={`btn btn-${props.theme} btn-sm m-1`} />
+        <SimpleButton {...childProps } className={`btn btn-${props.theme} btn-sm m-1`} />
     )
 }
