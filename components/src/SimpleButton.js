433cc6fb9e60fee014e497323fbfa7a06ad4875d
Marek Zdybel <marek@motta.com.pl>
2023-01-09 20:20:44 +0100

    Create component with state

--- components/src/SimpleButton.js
+++ components/src/SimpleButton.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export function SimpleButton(props) {
-    return (
-        <button onClick={ props.collback } className={ props.className }
-        disabled={ props.disabled === "true" || props.disabled === true }>
-            { props.text }
-        </button>
-    )
+export class SimpleButton extends Component {
+    render () {
+        return (
+            <button onClick={ this.props.collback }
+                    className={ this.props.className }
+                    disabled={ this.props.disabled === "true"
+                        || this.props.disabled === true }>
+                { this.props.text }
+            </button>
+        )
+    }
 }
