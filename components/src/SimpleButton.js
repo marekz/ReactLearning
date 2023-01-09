30f95d18030d52e414f8a963c61e9b258427cf6e
Marek Zdybel <marek@motta.com.pl>
2023-01-09 20:06:37 +0100

    Preparing to work with components with state

--- components/src/SimpleButton.js
+++ components/src/SimpleButton.js
@@ -3,19 +3,9 @@ import PropTypes from "prop-types";
 
 export function SimpleButton(props) {
     return (
-        <button onClick={props.collback} className={props.className}>
-            {props.text}
+        <button onClick={ props.collback } className={ props.className }
+        disabled={ props.disabled === "true" || props.disabled === true }>
+            { props.text }
         </button>
     )
-}
-
-SimpleButton.defaultProps = {
-    disabled: false
-}
-
-SimpleButton.propTypes = {
-    text: PropTypes.string,
-    theme: PropTypes.string,
-    callback: PropTypes.func,
-    disabled: PropTypes.oneOfType([PropTypes.bool, PropTypes.string])
 }
--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {CallbackButton} from "./CallbackButton";
+// import {CallbackButton} from "./CallbackButton";
+import {SimpleButton} from "./SimpleButton";
 
 export function Summary(props) {
     return <React.Fragment>
@@ -7,12 +8,15 @@ export function Summary(props) {
         <td>{props.name} </td>
         <td>{props.name.length} </td>
         <td>
-            <CallbackButton
-                callback={props.reverseCallback} />
-            <CallbackButton theme="info"
-                text="Na początek"
-                callback={() => props.promoteCallback(props.name)}
-                disabled={props.disabled === "true" || props.disabled === true}
+            <SimpleButton
+                className="btn btn-warning btn-sm m-1"
+                callback={ props.reverseCallback }
+                text={ `Odwróć kolejność (${props.name})` }
+            />
+            <SimpleButton
+                className="btn btn-info btn-sm m-1"
+                callback={ () => props.promoteCallback(props.name) }
+                text={ `Na początek (${props.name})` }
             />
         </td>
     </React.Fragment>
