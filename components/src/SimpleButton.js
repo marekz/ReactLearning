a9ab4d35c2f659f373691c4fc7fa70d57dc04497
Marek Zdybel <marek.zdybel@sbg.com.pl>
2023-01-10 16:42:57 +0100

    avoiding the dependent value problem

--- components/src/SimpleButton.js
+++ components/src/SimpleButton.js
@@ -26,10 +26,8 @@ export class SimpleButton extends Component {
     }
 
     handleClick = () => {
-        this.setState({
-            counter: this.state.counter + 1,
-            hasButtonBeenClicked: true
-        });
+        this.setState({ counter: this.state.counter + 1 },
+            () => this.setState({ hasButtonBeenClicked: this.state.counter > 0 }));
         this.props.callback();
     }
 }
