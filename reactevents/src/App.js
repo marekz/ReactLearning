9ca6d389977dffb1b3a869e3a4ea65b8d36cd598
Marek Zdybel <marek@motta.com.pl>
2023-01-18 01:40:34 +0100

    Simple event calling

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "Gotowy!"
+    }
+  }
+
+  render() {
+    return (
+        <div className="m-2">
+          <div className="h4 bg-primary text-white text-center p-2">
+            { this.state.message }
+          </div>
+          <div className="text-center">
+            <button className="btn btn-primary"
+                    onClick={() => this.setState({ message: "Kliknięty!" })}
+            >Kliknij mnie!</button>
+          </div>
+        </div>
+    )
+  }
 }
-
-export default App;
