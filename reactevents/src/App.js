483ad693157d41c0160f88ebab6668f0ab1ef6bb
Marek Zdybel <marek@motta.com.pl>
2023-01-24 19:09:47 +0100

    preventing default event handling

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -18,9 +18,20 @@ export default class App extends Component {
           },() => this.setState({ message: `${event.type}: ${this.state.counter}`}));
   }
 
+  toggleCheckBox = (event) => {
+      if (this.state.counter === 0) {
+          event.preventDefault()
+      }
+  }
+
   render() {
     return (
         <div className="m-2">
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox"
+                       onClick={this.toggleCheckBox} />
+                <label>To jest pole wyboru</label>
+            </div>
             <div className={`h4 bg-${this.state.theme}
                 text-white text-center p-2`}>
                 {this.state.message}
