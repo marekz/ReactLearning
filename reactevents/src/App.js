3bc8745a63958d7865b4463a5ff173c0bf869f32
Marek Zdybel <marek@motta.com.pl>
2023-01-24 18:52:25 +0100

    calling event handler methods with custom arguments - bad solution

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -5,27 +5,36 @@ export default class App extends Component {
     super(props);
     this.state = {
         message: "Gotowy!",
-        counter: 0
+        counter: 0,
+        theme: "secondary"
     }
   }
 
   handleEvent = (event) => {
       event.persist();
-      this.setState({ counter: this.state.counter + 1 },
-          () => this.setState({ message: `${event.type}: ${this.state.counter}`}));
+      this.setState({
+            counter: this.state.counter + 1,
+            theme: event.target.innerText === "normalny" ? "primary" : "danger"
+          },() => this.setState({ message: `${event.type}: ${this.state.counter}`}));
   }
 
   render() {
     return (
         <div className="m-2">
-          <div className="h4 bg-primary text-white text-center p-2">
-            { this.state.message }
-          </div>
-          <div className="text-center">
-            <button className="btn btn-primary"
-                    onClick={this.handleEvent}
-            >Kliknij mnie!</button>
-          </div>
+            <div className={`h4 bg-${this.state.theme}
+                text-white text-center p-2`}>
+                {this.state.message}
+            </div>
+            <div className="text-center">
+                <button className="btn btn-primary"
+                        onClick={this.handleEvent}>
+                    normalny
+                </button>
+                <button className="btn btn-danger m-1"
+                        onClick={this.handleEvent}>
+                    uwaga!
+                </button>
+            </div>
         </div>
     )
   }
