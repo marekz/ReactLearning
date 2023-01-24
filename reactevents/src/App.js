2f03ee00952910e68d67d1866040dd3b5007e4d2
Marek Zdybel <marek@motta.com.pl>
2023-01-24 20:06:30 +0100

    Event propagation management

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { ThemeButton } from "./ThemeButton";
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -10,41 +10,23 @@ export default class App extends Component {
     }
   }
 
-  handleEvent = (event, newTheme) => {
-      event.persist();
+  selectTheme = (newTheme) => {
       this.setState({
-            counter: this.state.counter + 1,
-            theme: newTheme
-          },() => this.setState({ message: `${event.type}: ${this.state.counter}`}));
-  }
-
-  toggleCheckBox = (event) => {
-      if (this.state.counter === 0) {
-          event.preventDefault()
-      }
+          theme: newTheme,
+          message: `Theme: ${newTheme}`
+      });
   }
 
   render() {
     return (
         <div className="m-2">
-            <div className="form-check">
-                <input className="form-check-input" type="checkbox"
-                       onClick={this.toggleCheckBox} />
-                <label>To jest pole wyboru</label>
-            </div>
             <div className={`h4 bg-${this.state.theme}
                 text-white text-center p-2`}>
                 {this.state.message}
             </div>
             <div className="text-center">
-                <button className="btn btn-primary"
-                        onClick={(e) => this.handleEvent(e, "primary")}>
-                    normalny
-                </button>
-                <button className="btn btn-danger m-1"
-                        onClick={(e) => this.handleEvent(e, "danger")}>
-                    uwaga!
-                </button>
+                <ThemeButton theme="primary" callback={this.selectTheme} />
+                <ThemeButton theme="danger" callback={this.selectTheme} />
             </div>
         </div>
     )
