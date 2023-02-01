1123162dc69c938f76328d5bf6f7908b8c306abc
Marek Zdybel <marek@motta.com.pl>
2023-02-01 23:32:55 +0100

    Explicit triggering of the reconciliation process

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Message } from "./Message";
 import { List } from "./List";
+import {ExternalCounter} from "./ExternalCounter";
 
 export default class App extends Component {
   constructor(props) {
@@ -18,13 +19,16 @@ export default class App extends Component {
     console.log("Komponent App, metoda render.");
     return <div className="container text-center">
       <div className="row p-2">
-        <div className="col-6">
+        <div className="col-4">
           <Message message={ `Licznik: ${this.state.counter}` }
                    callback={ this.incrementCounter }
                    text="Inkremetuj licznik" />
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <List />
+        </div>
+        <div className="col-4">
+          <ExternalCounter />
         </div>
       </div>
     </div>
