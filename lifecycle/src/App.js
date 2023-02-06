20569484174884f5387855df84916f57d42e56a8
Marek Zdybel <marek@motta.com.pl>
2023-02-06 22:43:52 +0100

    Set state data by props properties

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,45 +1,31 @@
 import React, { Component } from "react";
-import { Message } from "./Message";
+// import { Message } from "./Message";
 // import { List } from "./List";
 // import {ExternalCounter} from "./ExternalCounter";
 // import { HooksMessage } from "./HooksMessage";
+import { DirectionDisplay } from "./DirectionDisplay";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counterLeft: 0,
-      counterRight: 0
+      counter: 0,
     }
   }
 
-  incrementCounter = (counter) => {
-    if (counter === "left") {
-      this.setState({ counterLeft: this.state.counterLeft + 1 });
-    } else {
-      this.setState({ counterRight: this.state.counterRight + 1 });
-    }
-  }
-
-  handleChange = () => {
-    this.setState({ showMessage: !this.state.showMessage })
+  changeCounter = (val) => {
+    this.setState({counter: this.state.counter + val })
   }
 
   render() {
     console.log("Komponent App, metoda render.");
     return <div className="container text-center">
-      <div className="row p-2">
-        <div className="col-6">
-          <Message message={ `Lewy: ${this.state.counterLeft}` }
-                   callback={ () => this.incrementCounter("left") }
-                   text="Inkremetuj lewy licznik" />
-        </div>
-        <div className="col-6">
-          <Message message={ `Lewy: ${this.state.counterRight}` }
-                   callback={ () => this.incrementCounter("right") }
-                   text="Inkremetuj prawy licznik" />
-        </div>
-      </div>
+      <DirectionDisplay value={this.state.counter} />
+      <button className="btn btn-primary m-1"
+              onClick={() => this.changeCounter(-1)}>Pomniejsz</button>
+      <button className="btn btn-primary m-1"
+              onClick={() => this.changeCounter(1)}>Powiększ</button>
+
     </div>
   }
 }
