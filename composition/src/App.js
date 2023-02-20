3d6ab018df28ad51a3af99d4f0ddbf550ee2377d
Marek Zdybel <marek@motta.com.pl>
2023-02-20 18:51:10 +0100

    Add some components

--- composition/src/App.js
+++ composition/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { ActtionButton } from "./ActionButton";
+import { Message } from "./Message";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1});
+  }
+
+  render() {
+    return <div className="m-2 text-center">
+      <Message theme="primary"
+        message={`Licznik: ${this.state.counter}`} />
+      <ActtionButton theme="secondary"
+        text="Inkrementuj" callback={this.incrementCounter} />
     </div>
-  );
+  }
 }
-
-export default App;
