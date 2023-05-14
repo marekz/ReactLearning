3db23ea96c2b67a7854f93d8aa2f1636b3d87d35
Marek Zdybel <marek@motta.com.pl>
2023-05-14 14:01:03 +0200

    Adding the props property to the child components of the container

--- composition/src/ThemeSelector.js
+++ composition/src/ThemeSelector.js
@@ -1,11 +1,36 @@
 import React, {Component} from "react";
 
 export class ThemeSelector extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            theme: "primary"
+        }
+        this.theme = ["primary", "secondary", "success", "warning", "dark"];
+    }
+
+    setTheme = (event) => {
+        this.setState({ theme: event.target.value });
+    }
+
     render () {
+
+        let modChildren = React.Children.map(this.props.children,
+            (c => React.cloneElement(c, { theme: this.state.theme })));
+
         return (
             <div className="bg-dark p-2">
+                <div className="form-group text-left">
+                    <label className="text-white">Temat: </label>
+                    <select className="form-control" value={this.state.theme}
+                            onChange={this.setTheme}>
+                        {this.theme.map(theme =>
+                            <option key={theme} value={theme}>{theme}</option>)}
+                    </select>
+                </div>
                 <div className="bg-info p-2">
-                    {this.props.children}
+                    { modChildren }
                 </div>
             </div>
         )
